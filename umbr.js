0c9941d0965f93b1da233340890c84f608a70ee5
ZippyMagician <zippymagician1@gmail.com>
2021-09-29 14:16:02 -0400

    Some additions

--- umbr.js
+++ umbr.js
@@ -7,8 +7,17 @@
 //  4 -> arr
 //  5 -> ;
 //  6 -> :
+//  7 -> var
+//  8 -> group
+//  9 -> whitespace
 function A(d,r,b=0){this.r=typeof r==='number'?[r]:r;this.ds=this.r.length;this.d=d;this.b=b;
 fix(this)}
+function I(fn,m){this.m=m;this.fn=fn;this.incomp=1;}
+I.prototype.bind = function(v){
+  this.m=1;
+  this.fn=this.fn.bind(v);
+  return this;
+}
 A.prototype.toString = function(){
   let S="";
   let r=this.r.reverse();
@@ -32,9 +41,23 @@ A.prototype.toString = function(){
   }
   return S;
 }
-const syms="+-".split("")
-     ,bdrs='."'.split("")
-
+const syms={
+  "+":[a=>+a,(a,b)=>a+b],
+  "-":[a=>-a,(a,b)=>a-b],
+}
+,bdrs={
+  '.':[(a,b)=>r=>a(b(r)),(a,b)=>(l,r)=>a(l,b(r))],
+  '"':[(a,b)=>r=>a(b(r)),(a,b)=>(l,r)=>a(b(l,r))]
+}
+,err=id=>{
+  switch(id){
+    case 0:console.error("[0] ARG ERROR");break;
+    case 1:console.error("[1] RANK ERROR");break;
+    case 2:console.error("[2] DOMAIN ERROR");break;
+    case 3:console.error("[3] NAME ERROR");break;
+  }
+  process.exit(1);
+}
 ,chnk=(a,s)=>{
   let n=[],
       b=0;
@@ -49,17 +72,23 @@ const syms="+-".split("")
 }
 ,str=s=>s.toString()
 ,resc=r=>r.replace(/[^A-Za-z0-9_]/g,'\\$&')
+,nnw=(t,i)=>{
+  let o=1;
+  while(t[i+o].t==9)o++;
+  return [i+o,t[i+o]];
+}
 ,lex=s=>{
   let test,
       toks=[];
   while(s){
-    if(test=/^(-?[0-9]*\.?[0-9]+)/.exec(s))toks.push({t:0,v:+test[1]});
+    if(test=/^(_?[0-9]*\.?[0-9]+)/.exec(s))toks.push({t:0,v:+test[1].replace(/_/g,'-')});
     else if(test=/^'((?:[^']|\\')*)'/.exec(s))toks.push({t:1,v:test[1].split("").map(c=>c.charCodeAt(0))});
     else if(test=/^;/.exec(s))toks.push({t:5});
     else if(test=/^:/.exec(s))toks.push({t:6});
-    else if(test=RegExp(`^(${syms.map(resc).join('|')})`).exec(s))toks.push({t:2,v:test[1]});
-    else if(test=RegExp(`^(${syms.map(bdrs).join('|')})`).exec(s))toks.push({t:3,v:test[1]});
-    s=s.slice(test[0].length).trim();
+    else if(test=RegExp(`^(${Object.keys(syms).map(resc).join('|')})`).exec(s))toks.push({t:2,v:test[1]});
+    else if(test=RegExp(`^(${Object.keys(bdrs).map(resc).join('|')})`).exec(s))toks.push({t:3,v:test[1]});
+    else if(test=/^\n|\s/.exec(s))toks.push({t:9,v:test[0]});
+    s=s.slice(test[0].length);
   }
   return toks;
 },
@@ -68,13 +97,32 @@ strand=t=>{
       b=[];
   for(let i=0;i<=t.length;i++)
     if(t[i]!=null&&t[i].t<2)b.push(t[i]);
-    else if(b.length==1)tn.push(...(t[i]!=null?[b.pop(),t[i]]:[b.pop()]));
-    else {
+    else if((t[i]==null||t[i]>=2||t[i].t==9&&t[i].v=='\n')&&b.length==1)tn.push(...(t[i]!=null?[b.pop(),t[i]]:[b.pop()]));
+    else if(t[i]==null||!(t[i].t==9&&t[i].v==' ')){
       for(x in b)if(b[x].t==1)b[x]=new A(b[x].v,[b[x].v.length],1);else b[x]=b[x].v;
       let a=new A(b,b.length,0);
       tn.push(...(t[i]!=null?[{t:4,v:a},t[i]]:[{t:4,v:a}]))
       b=[]
     }
   return tn;
+},
+exec=t=>{
+  let q=[],
+      fq=[],
+      env={};
+  for(let i=0;i<t.length;i++){
+    let o=t[i];
+    if(o.t==7){
+      if(nnw(t,i)[1].t==6){[i,]=nnw(t,i);} // TODO: declaration
+      else if(env[o.v]&&env[o.v].incomp){
+        if(env[o.v].m==1)fq.push(env[o.v]);
+        else if(!q.length)err(0);
+        else fq.push(env[o.v].bind(q.pop()));
+      } else if(env[o.v]!=null)q.push(env[o.v]);
+      else err(3);
+    }else if(o.t==2){
+
+    }
+  }
 }
-console.log(strand(lex(process.argv[2])));
+exec(strand(lex(process.argv[2])))
