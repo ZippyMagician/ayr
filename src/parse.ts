432c2f2c25de5703b56f11163fcef929bd4c44bb
ZippyMagician <zippymagician1@gmail.com>
2025-10-09 16:29:42 -0400

    parsing maintains newlines in node stream

--- src/parse.ts
+++ src/parse.ts
@@ -22,6 +22,11 @@ function is_whitespace(type: TokenIdent, accept_separator: boolean = true): bool
     return type == TokenIdent.Space || accept_separator && type == TokenIdent.Separator;
 }
 
+function is_line_end(tokens: Token[], i: number): boolean {
+    let { ident, value } = tokens[i] ?? eof();
+    return ident == TokenIdent.Separator && value.as_str() == "\n" || ident == TokenIdent.EOF;
+}
+
 // Next No Whitespace -- Returns [Token, index] where index is the location of the Token in the Token stream
 function nnw(tokens: Token[], from: number, accept_separator: boolean = true): [Token, number] {
     let i = from;
@@ -39,12 +44,16 @@ export const enum NodeType {
     Literal,
 
     Block,
+
+    // Line Separator
+    Line,
 }
 
 // Operator, Symbol, Block TODO
 export type Node =
     [ NodeType.Instant, Value ]  |
-    [ NodeType.Literal, string ]
+    [ NodeType.Literal, string ] |
+    [ NodeType.Line, "\n" ]
 
 // TODO: Figure out how to parse tokens
 // Need to separate literals into arrays.
@@ -85,8 +94,9 @@ export function parse_nodes(tokens: Token[]): Node[] {
     let i = 0;
 
     while (i < tokens.length) {
-        let [head, j] = nnw(tokens, i);
+        let [head, j] = nnw(tokens, i, false);
         if (is_instant(tokens, j)) {
+            // An instant value
             let list: Token[] = [];
             let intermediary: (Value | Num)[] = [];
             let next: Token;
@@ -123,10 +133,20 @@ export function parse_nodes(tokens: Token[]): Node[] {
                             ) : values.length == 1 ? (values[0]! as Value).as_list()[0]! as Value : 
                                                      primitive(values)
             ]);
+            
             i = j;
+            if (is_line_end(tokens, i)) stream.push([NodeType.Line, "\n"]); // Add trailing newline
         } else if (head.ident == TokenIdent.LParen) {
+            // Left parens denote a group. A train if parser reaches this branch.
             let [ inst, group, j ] = get_group(tokens, i);
             err(-1, "TODO: Non instant groups.");
+            i = j;
+        } else if (is_line_end(tokens, j)) {
+            // Line separator (right → left, top → bottom parse order)
+            stream.push([NodeType.Line, "\n"]);
+            i = j;
+        } else if (head.ident == TokenIdent.Separator) {
+            // Other separators are syntactically insignificant in this branch.
             i = j;
         } else {
             err(-1, "TODO");
