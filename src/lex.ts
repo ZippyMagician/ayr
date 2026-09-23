61ceb0e4b76dca8461f197acd55ebe16db0c25c6
Joshua Barnett <zippymagician1@gmail.com>
2026-09-23 12:32:27 -0400

    Internals now have trailing '.', not leading.

--- src/lex.ts
+++ src/lex.ts
@@ -125,8 +125,8 @@ export function lex(str: string): Token[] {
             push(match[0] == " " ? TokenIdent.Space : TokenIdent.Separator, match[1]!);
         else if (match = /^(NL\.)/.exec(str)) push(TokenIdent.Separator, "\n");
         // Literals (environment variables & user defined variables
+        else if (match = /^([a-zA-Z][a-zA-Z_]*)\./.exec(str)) push(TokenIdent.InternalLit, match[1]!);
         else if (match = /^([a-zA-Z][a-zA-Z_]*)/.exec(str)) push(TokenIdent.Literal, match[1]!);
-        else if (match = /^\.([a-zA-Z][a-zA-Z_]*)/.exec(str)) push(TokenIdent.InternalLit, match[1]!);
         else err(3, `Unrecognized token starting at: '${str.slice(0, 5)}...'.`);
 
         str = str.slice(match && match[0].length || 1);
